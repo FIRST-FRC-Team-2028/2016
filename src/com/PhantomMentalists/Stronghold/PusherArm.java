deddaeb41a0eeb0df738e1220027354c841e56a0
Austin <spidey213a@gmail.com>
2016-02-05 20:09:48 -0500

    Filled in constructer

--- src/com/PhantomMentalists/Stronghold/PusherArm.java
+++ src/com/PhantomMentalists/Stronghold/PusherArm.java
@@ -3,12 +3,16 @@ package com.PhantomMentalists.Stronghold;
 import com.PhantomMentalists.Stronghold.Parameters;
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 
 /**
- * <p>Author: Ryan</p>
+ * <p>Author: Austin</p>
  */
 @objid ("a38fcf5c-bc25-4155-8afc-05a8335fee44")
-public class PusherArm {
+public class PusherArm
+{
+	CANTalon positionMotor;
     /**
      * <Enter note text here>
      */
@@ -16,19 +20,32 @@ public class PusherArm {
     protected CANTalon tiltMotor;
 
     @objid ("ef54315f-71a9-48b1-8840-4441e6e9db2c")
-    public PusherArm() {
+    public PusherArm()
+    {
+    	positionMotor = new CANTalon(0);
+		positionMotor.changeControlMode(TalonControlMode.Position);
+		positionMotor.enableBrakeMode(true);
+		positionMotor.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		positionMotor.setPID(0, 0, 0);
+		positionMotor.enable();
     }
 
     @objid ("59a12327-d8c3-4b2c-8521-285b79fef074")
-    public void manualRaise() {
+    public void manualRaise()
+    {
+    	
     }
 
     @objid ("bea74f7c-38ea-4673-bb5d-d27ab80f34de")
-    public void manualLower() {
+    public void manualLower()
+    {
+    	
     }
 
     @objid ("08cddce2-8b6f-4b33-aaab-0e3e90c8fed4")
-    public void manualStop() {
+    public void manualStop()
+    {
+    	
     }
-
+    
 }
