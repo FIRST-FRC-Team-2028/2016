5dacb5abbbceced565ebdaeb2c84ee968fe73ebb
Tom Dunn <dunn@Toms-MacBook-Air.local>
2016-02-06 12:57:45 -0500

    Updated Shooter and ClimbingArm

--- src/com/PhantomMentalists/Stronghold/ClimbingArm.java
+++ src/com/PhantomMentalists/Stronghold/ClimbingArm.java
@@ -18,6 +18,12 @@ public class ClimbingArm {
     @objid ("585b27f8-06a7-403e-a648-3ca745308acc")
     protected CANTalon raiseLowerMotor;
 
+    @objid ("71341c60-1263-46a0-9d0c-01aae0262ed7")
+    protected CANTalon leftWenchMotor;
+
+    @objid ("c01aa643-829c-4e75-aadb-53ff254d418b")
+    protected CANTalon rightWenchMotor;
+
     @objid ("c215a8ac-a925-4c23-aab7-ec75ee354734")
     public ClimbingArm() {
     }
