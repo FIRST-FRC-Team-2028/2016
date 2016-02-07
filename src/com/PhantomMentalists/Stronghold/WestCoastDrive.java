c01002d1321eee9ed432b690f1adc9f96860bd3c
robotics <robotics@10.11.14.141>
2016-02-07 17:18:02 -0500

    Made some changes and commented everything

--- src/com/PhantomMentalists/Stronghold/WestCoastDrive.java
+++ src/com/PhantomMentalists/Stronghold/WestCoastDrive.java
@@ -1,5 +1,7 @@
 package com.PhantomMentalists.Stronghold;
-
+/**
+ * imports
+ */
 import com.PhantomMentalists.Stronghold.DriveUnit.Gear;
 import com.PhantomMentalists.Stronghold.DriveUnit.Placement;
 import com.PhantomMentalists.Stronghold.Parameters;
@@ -9,32 +11,39 @@ import edu.wpi.first.wpilibj.PIDOutput;
 /**
  * <p>Author: Zavion</p>
  */
-@objid ("bfe07845-b9da-40e9-a047-c801ee86b209")
-public class WestCoastDrive implements PIDOutput
+	  @objid ("bfe07845-b9da-40e9-a047-c801ee86b209")
+	public class WestCoastDrive implements PIDOutput
 
 {
 	
     /**
-     * <Enter note text here>
+     * <sets the turn setpoint>
      */
-    @objid ("994a7329-7dde-4b3a-af65-88988500092f")
+      @objid ("994a7329-7dde-4b3a-af65-88988500092f")
     public double turnSetpoint;
-
+    
     /**
-     * <Enter note text here>
+     * its a gear 
      */
-    @objid ("28bc5bec-15fd-4924-b01c-5f725341e189")
+      @objid ("28bc5bec-15fd-4924-b01c-5f725341e189")
     protected Gear gear = Gear.kLowGear;
-
-    @objid ("b8a106fc-3334-47d0-b500-cf128b91b6c0")
+    /**
+     * left side
+     */
+      @objid ("b8a106fc-3334-47d0-b500-cf128b91b6c0")
 	protected DriveUnit leftSide;
-
-	
-    @objid ("d0db5703-a6c1-4909-baf4-37eb81a88730")
+  
+	/**
+	 * Right side
+	 */
+      @objid ("d0db5703-a6c1-4909-baf4-37eb81a88730")
     protected DriveUnit rightSide;
-
-    @objid ("06e23f53-7280-4c29-b319-50a651aca613")
+   /**
+    * constructor
+    */
+      @objid ("06e23f53-7280-4c29-b319-50a651aca613")
     public WestCoastDrive() 
+    
     	{
     	leftSide = new DriveUnit(Placement.Left);
     	rightSide = new DriveUnit(Placement.Right);
@@ -46,44 +55,62 @@ public class WestCoastDrive implements PIDOutput
      * Set the output to the value calculated by PIDController
      * @param output the value calculated by PIDController
      */
-    @objid ("fc5f1ae8-f7bf-4800-924d-f54e5ee2a661")
+      @objid ("fc5f1ae8-f7bf-4800-924d-f54e5ee2a661")
     public void pidWrite(double output) 
-    
-    {}
-
-    @objid ("efa356b3-f44c-44ee-a418-1ba6be974be5")
-    public void setSetpoint(double setPoint)
-    
+    /**
+     * Instantiates a PIDCommand that will use the given p, i and d values. It will use the class name as its name. 
+     */
     	{
+    	 PIDCommand String;
+                double p;
+                double i;
+                double d;
+                double period;
+    	}
+/**
+ * sets the set point
+ * @param setPoint
+ */
+      @objid ("efa356b3-f44c-44ee-a418-1ba6be974be5")
+    public void setSetpoint(double setPoint)
+   
+    	{
+    	
     	leftSide.setSpeedSetpoint(setPoint);
     	rightSide.setSpeedSetpoint(setPoint);
+    	
     	}
-
-    @objid ("ea714300-5ea7-4831-acb5-2503084baa47")
+/**
+ * gets the set point
+ * @return
+ */
+      @objid ("ea714300-5ea7-4831-acb5-2503084baa47")
     public double getSetpoint()
     
     	{
-    	
-        	return 0;
+    		return 0;
     	}
     
-
-    @objid ("77501865-67e2-4889-b074-f047bd3616a6")
+     /**
+      * gets gear and return get value
+      * @return 
+      */
+      @objid ("77501865-67e2-4889-b074-f047bd3616a6")
     Gear getGear() 
-    
-    	{
-    // Automatically generated method. Please delete this comment before entering specific code.
-        	return this.gear;
-    	}
-
-    @objid ("3e825af2-8a08-4402-9058-dd7ae4aa2260")
+      	
+      	{
+    	  	return this.gear;
+        }
+    /**
+     * This set the left and right gear based on the gear value
+     * @param value
+     */
+      @objid ("3e825af2-8a08-4402-9058-dd7ae4aa2260")
     void setGear(Gear value) 
-    
-    	{
-        // Automatically generated method. Please delete this comment before entering specific code.
-        gear = value;
-    	leftSide.setGear(gear);
-    	rightSide.setGear(gear);
+        {
+    	  gear = value;
+    		leftSide.setGear(gear);
+    		rightSide.setGear(gear);
     	}
 
 }
